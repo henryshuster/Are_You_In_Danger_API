80802e33a75b71152239c055a28d2fc04cc6972b
Shuster <henry.shuster19@mbpr1315-1785.trinity.nyc.ny.us>
2019-05-01 08:29:48 -0400

    key

--- controllers/key.js
+++ controllers/key.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Shooting = require('../models/shooting');
 
 router.get('/key/new', function(req,res){
 console.log("New API Key Requested");
@@ -9,11 +10,15 @@ res.render('key');
 });
 
 router.post('/key/:id', function(req,res){
-var key=0;
+var user_data={};
+user_data["email"]=req.body.email.trim();
+user_data["password"]=req.body.password.trim();
 
-res.status(200);
-res.setHeader('Content-Type', 'text/html');
-res.render('index', key);
+Shooting.createUser(user_data, function(u){
+  res.status(200);
+  res.setHeader('Content-Type', 'text/html');
+  res.render('index', {key:user_data});
+});
 });
 
 module.exports = router;
