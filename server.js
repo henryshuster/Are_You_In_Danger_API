e0dfadf2bab1708b785bb9aa7cfca4787e124454
Henry Shuster <henryshuster@Henrys-MBP-2.fios-router.home>
2019-05-10 21:32:22 -0400

    dada

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var fs = require('fs');
 
 var app = express();
 
-var port = process.env.PORT||8000;
+var port = process.env.PORT || 8000;
+
+
 
 app.use(express.static('public'));
 app.set('views', __dirname + '/views');
@@ -12,7 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.use(require('./controllers/key'));
-app.use(require('./controllers/data'));
 
 app.listen(port, function(){
   console.log('Server started at '+ new Date()+', on port ' + port+'!');
